424e0116aa40987b2b8982b500a3a86879d4a548
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-02-18 16:05:57 +0800

    Added integration tests for Landings controller

--- test/controllers/landings_controller_test.rb
+++ test/controllers/landings_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class LandingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get landings_index_url
+  # test "should get index" do
+  #   get landings_index_url
+  #   assert_response :success
+  # end
+
+  test "should get to root page" do
+    get root_path
     assert_response :success
   end
+
 end
